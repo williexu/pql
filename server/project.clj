fc5c1a8e36ded571a0916a4a266761c75977b1f1
Wyatt Alt <wyatt.alt@gmail.com>
2019-01-18 08:06:17 -0800

    Bump server version

--- server/project.clj
+++ server/project.clj
@@ -1,4 +1,4 @@
-(defproject pqlserver "0.0.2-SNAPSHOT"
+(defproject pqlserver "0.0.3-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
